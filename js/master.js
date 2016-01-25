ed6276252dd378e9fb7529ac478b69bfef7b0ca0
Isabelle George and Tim Durward <timothy.a.durward@gmail.com>
2016-01-25 09:19:10 -0800

    finish user input

--- js/master.js
+++ js/master.js
@@ -6,5 +6,19 @@ function Place(location, landmark, season){
 
 
 $(document).ready(function(){
+  $('#buttonSubmit').click(function() {
+    var location = $('input#placeForm').val();
+    var landmark = $('input#landmarkForm').val();
+    var season = $('input#seasonForm').val();
+    var newPlace = new Place(location, landmark, season);
 
+    $('.locationNameList').append("<li><span class='locationNames'>" + newPlace.location + "</span></li>");
+
+    $('.locationNames').last().click(function() {
+      $('#showLocation').show();
+      $('.landmarkInfo').text(newPlace.landmark);
+      $('.seasonInfo').text(newPlace.season);
+    });
+    event.preventDefault();
+  });
 });
