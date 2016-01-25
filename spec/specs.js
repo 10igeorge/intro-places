2ae4ac2a7fd90fc71f0ffcb56a2a2cb57524e2f5
Isabelle George and Tim Durward <timothy.a.durward@gmail.com>
2016-01-25 08:15:07 -0800

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,11 @@
+describe('Place', function() {
+  it('will pass true if all objects match constructor', function() {
+    var newPlace = new Place("Vancouver, BC", "Tim's House", "Winter");
+    expect(newPlace.location).to.equal("Vancouver, BC");
+    expect(newPlace.landmark).to.equal("Tim's House");
+    expect(newPlace.season).to.equal("Winter");
+  });
+});
 
 // Example Spec below:
 
